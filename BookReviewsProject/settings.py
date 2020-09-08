b48bb4f966a3cd3e372880ec12e78e22fffa6552
kunxin-chor <chorkunxin@yahoo.com>
2020-09-08 02:50:19 +0000

    Change database settings

--- BookReviewsProject/settings.py
+++ BookReviewsProject/settings.py
@@ -94,16 +94,16 @@ WSGI_APPLICATION = 'BookReviewsProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
